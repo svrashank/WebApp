9ed3c5c76af7ea9abac7f46cae54132834146dfb
Vrashank Shetty <svrashank@gmail.com>
2023-11-14 18:53:20 +0530

    Priliminary layouts for "Browse" and "Home'

--- webpage.py
+++ webpage.py
@@ -1,17 +1,11 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, url_for
 
 app = Flask(__name__)
-posts = [{"Project_Name":"Mumbai Weather",
-          "Modules":"sklearn, matplotlib, Pandas",
-          "Summary":"Shows how the weather has changed over last 3 decades"},
 
-          {"Project_Name":"Webscraping Finances of Companies",
-          "Modules":"Requests, BeautifulSoup, Pandas",
-          "Summary":"Webscraping major financial indicators of Nifty 50 companies from Yahoo Finance"}]
 
 @app.route("/home")
-def projects():
-    return render_template("home.html",posts = posts)
+def home():
+    return render_template("home.html")
 
 @app.route("/qualifications")
 def explanations():
