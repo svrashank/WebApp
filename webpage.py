b1f9696181cc64c2e9c8552fea6974a1cda67d91
Vrashank Shetty <svrashank@gmail.com>
2023-12-02 13:34:07 +0530

    Basic Layout for home page

--- webpage.py
+++ webpage.py
@@ -2,6 +2,9 @@ from flask import Flask,render_template, url_for
 
 app = Flask(__name__)
 
+posts = [{"Name":"Vrashank Shetty","Skills":"Data Cleaning|EDA|Web Scraping ","Experience":"Fresher"},
+         {"Name":"Jane Doe","Skills":"Model building|EDA|Web Development","Experience":"4 Years"}
+         ]
 
 @app.route("/")
 def sign_in():
@@ -17,7 +20,7 @@ def qualifications():
 
 @app.route("/home")
 def home():
-    return render_template("home.html")
+    return render_template("home.html",posts=posts)
 
 @app.route("/yourprofile")
 def yourprofile():
